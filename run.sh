333f7a98bcb377039c1d92bc883e3697e91196b5
AngusWu <anguswu@simbo.com.tw>
2020-03-08 19:03:01 +0800

    add top.sh

--- run.sh
+++ run.sh
@@ -5,4 +5,9 @@ if [[ -n $(docker ps -aq --filter=name='play-async-profiler') ]]; then
     docker rm -f play-async-profiler
 fi
 
-docker run -d -p 9090:9090 --name play-async-profiler play-async-profiler:0.1.0
+docker run -d -p 9090:9090 --name play-async-profiler \
+--pid=host \
+--userns=host \
+--security-opt seccomp=unconfined \
+--cap-add SYS_ADMIN \
+play-async-profiler:0.1.0
